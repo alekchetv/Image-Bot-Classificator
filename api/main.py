7575cffe1ed3c1be3372c2b877cfe422388a04e8
Aleksey Chetveruhin <shnyrok2002@gmail.com>
2025-08-09 00:55:14 +0300

    deploy

--- api/main.py
+++ api/main.py
@@ -20,4 +20,4 @@ async def predict(file: UploadFile = File(...)):
     return {"Status": "Ok", "prediction": predict}
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
